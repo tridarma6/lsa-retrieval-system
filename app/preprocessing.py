fe3033e1eff44b2b27519cbadb78b129e23bf8ab
PutuRifkiDy <puturifki56@gmail.com>
2025-04-30 11:30:58 +0800

    update

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import nltk
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from nltk.stem import PorterStemmer  
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -12,7 +12,7 @@ nltk.download('stopwords')
 # --- Inisialisasi tokenizer, stopwords, dan stemmer ---
 tokenizer = RegexpTokenizer(r'\w+')
 stop_words = set(stopwords.words('english'))
-stemmer = StemmerFactory().create_stemmer()
+stemmer = PorterStemmer()
 
 # --- Fungsi Preprocessing ---
 def preprocess(text):
@@ -29,29 +29,29 @@ documents = df['summaries'].astype(str).tolist()
 tokenized_documents = [preprocess(doc) for doc in documents]
 
 # # --- Tampilkan Hasil Preprocessing 5 Baris Abstract ---
-# for i, doc in enumerate(tokenized_documents[:5], 1):
-#     print(f"Dokumen {i}:\n{doc}\n")
+for i, doc in enumerate(tokenized_documents[:5], 1):
+    print(f"Abstract {i}:\n{doc}\n")
 
-# --- Gabungkan token jadi teks untuk setiap dokumen ---
-cleaned_documents = [' '.join(doc) for doc in tokenized_documents]
+# # --- Gabungkan token jadi teks untuk setiap dokumen ---
+# cleaned_documents = [' '.join(doc) for doc in tokenized_documents]
 
-# --- TF-IDF Vectorization ---
-vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform(cleaned_documents)
+# # --- TF-IDF Vectorization ---
+# vectorizer = TfidfVectorizer()
+# tfidf_matrix = vectorizer.fit_transform(cleaned_documents)
 
-# --- Input Query dari Pengguna ---
-query = input("Masukkan query pencarian berupa abstract: ")
-preprocessed_query = preprocess(query)
-query_string = ' '.join(preprocessed_query)
-query_vector = vectorizer.transform([query_string])
+# # --- Input Query dari Pengguna ---
+# query = input("Masukkan query pencarian berupa abstract: ")
+# preprocessed_query = preprocess(query)
+# query_string = ' '.join(preprocessed_query)
+# query_vector = vectorizer.transform([query_string])
 
-# --- Hitung Cosine Similarity antara Query dan Abtract ---
-similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
+# # --- Hitung Cosine Similarity antara Query dan Abtract ---
+# similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
 
-# --- Tampilkan Top 5 Abstract Paling Relevan Dengan Query dan Hasil Paling Mirip ---
-df['Similarity Score'] = similarities
-df_result = df.sort_values(by='Similarity Score', ascending=False)
+# # --- Tampilkan Top 5 Abstract Paling Relevan Dengan Query dan Hasil Paling Mirip ---
+# df['Similarity Score'] = similarities
+# df_result = df.sort_values(by='Similarity Score', ascending=False)
 
-print(f"\nQuery: {query}")
-print("\nTop 5 Abstract Paling Relevan Dengan Query:")
-print(df_result[['titles', 'Similarity Score']].head(5))    
+# print(f"\nQuery: {query}")
+# print("\nTop 5 Abstract Paling Relevan Dengan Query:")
+# print(df_result[['titles', 'Similarity Score']].head(5))    
